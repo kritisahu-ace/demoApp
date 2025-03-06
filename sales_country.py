5ba6a0f0157ee168f3d16c3a0dfb7cc8a141419e
kritisahu-ace <kriti.sahu@accionlabs.com>
2025-03-06 20:23:06 +0530

    Update sales_country.py

--- sales_country.py
+++ sales_country.py
@@ -12,6 +12,8 @@ def load_data():
     # Construct the file path dynamically
     current_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(current_dir, "used_cars_india.csv")
+    # file_path = "used_cars_india.csv"
+
     df = pd.read_csv(file_path, sep="\t")
     df['Sales Date'] = pd.to_datetime(df['Sales Date'])
     return df
@@ -26,18 +28,18 @@ def forecast_sarima(series, order=(1, 1, 1), seasonal_order=(1, 1, 1, 12)):
         return 0  # Return 0 if model fitting fails
 
 def country_analysis():
-    df = load_data()
-    df_agg = df.groupby(['Model', 'Country', pd.Grouper(key='Sales Date', freq='Q')]).size().reset_index(name='Sales')
-    df_pivot = df_agg.pivot(index='Sales Date', columns=['Model', 'Country'], values='Sales').fillna(0)
+    # df = load_data()
+    # df_agg = df.groupby(['Model', 'Country', pd.Grouper(key='Sales Date', freq='Q')]).size().reset_index(name='Sales')
+    # df_pivot = df_agg.pivot(index='Sales Date', columns=['Model', 'Country'], values='Sales').fillna(0)
 
-    results = Parallel(n_jobs=-1)(delayed(forecast_sarima)(df_pivot[column]) for column in df_pivot.columns)
-    forecast_df = pd.DataFrame([(column[0], column[1], result) for column, result in zip(df_pivot.columns, results)],
-                               columns=['Model', 'Country', 'Forecasted Sales'])
+    # results = Parallel(n_jobs=-1)(delayed(forecast_sarima)(df_pivot[column]) for column in df_pivot.columns)
+    # forecast_df = pd.DataFrame([(column[0], column[1], result) for column, result in zip(df_pivot.columns, results)],
+    #                            columns=['Model', 'Country', 'Forecasted Sales'])
 
-    top_models = forecast_df.nlargest(10, 'Forecasted Sales')
-    top_models.sort_values('Forecasted Sales', inplace = True, ascending=False)
-    top_models['Model By Country'] = top_models['Model'] + " (" + top_models['Country'] + ")"
-
+    # top_models = forecast_df.nlargest(10, 'Forecasted Sales')
+    # top_models.sort_values('Forecasted Sales', inplace = True, ascending=False)
+    # top_models['Model By Country'] = top_models['Model'] + " (" + top_models['Country'] + ")"
+    top_models= pd.read_csv("top_models.csv")
     chart = (
         alt.Chart(top_models)
         .mark_bar()
